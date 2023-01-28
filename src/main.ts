192adcaa4f1f5fdb35521113a36533aff7479ede
Kirill <polozenko.kirill.job@gmail.com>
2023-01-28 19:49:10 +0200

    added enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { CorsMiddleware } from './common/middlewares/cors.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' });
   app.use(new CorsMiddleware().use);
   await app.listen(3000);
 }
